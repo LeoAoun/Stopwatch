f6ca645b06808139a73eb649b948b6fa2a7e5188
Leonardo Aoun <100950151+LeoAoun@users.noreply.github.com>
2022-08-12 14:01:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
 
-var hr = 0
-var min = 0
-var sec = 0
-var interval
+let hr = 0
+let min = 0
+let sec = 0
+let ms = 0
+let interval
 
-function counter(){
-    sec++
-    if(sec==60){
+function counter() {
+    ms+=10
+
+    if (ms == 1000) {
+        sec++
+        ms = 0
+    }
+    else if (sec == 60) {
         min++
-        sec=0
+        sec = 0
     }
-    if(min==60){
+    else if (min == 60) {
         hr++
-        min=0 
+        min = 0
     }
-    document.getElementById("counter").innerHTML=  twoDigits(hr) +":" + twoDigits(min) + ":" + twoDigits(sec)
+    document.getElementById("counter").textContent = twoDigits(hr) + ":" + twoDigits(min) + ":" + twoDigits(sec) + "." + threeDigits(ms)
 }
-function pause(){
+
+function pause() {
     clearInterval(interval)
+    document.getElementById('start').addEventListener('click', start)
 }
 
-function start(){
-    counter()
-    interval = setInterval(counter, 1000)
+function start() {
+    interval = setInterval(counter, 10)
+    document.getElementById('start').removeEventListener('click', start)
 }
 
-function stop(){
+function stop() {
     clearInterval(interval)
     hr = 0
     min = 0
     sec = 0
-    document.getElementById("counter").innerHTML=  twoDigits(hr) +":" + twoDigits(min) + ":" + twoDigits(sec)
+    ms = 0
+    document.getElementById("counter").textContent = twoDigits(hr) + ":" + twoDigits(min) + ":" + twoDigits(sec) + "." + threeDigits(ms)
+    document.getElementById('start').addEventListener('click', start)
 }
 
-function twoDigits(digit){
-    if(digit<10){
-        return('0'+digit)
-    }else{
+function twoDigits(digit) {
+    if (digit < 10) {
+        return ('0' + digit)
+    } else {
+        return (digit)
+    }
+}
+
+function threeDigits(digit) {
+    if (digit < 10) {
+        return ('00' + digit)
+    } 
+    else if(digit >= 10 && digit < 100){
+        return ('0' + digit)
+    }
+    else{
         return(digit)
     }
 }
+
+document.getElementById('pause').addEventListener('click', pause)
+document.getElementById('start').addEventListener('click', start)
+document.getElementById('stop').addEventListener('click', stop)
 
 
 
